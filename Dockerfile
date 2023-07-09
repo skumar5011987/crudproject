5e1b8a67e107b2ef33b60ed7d1465f50dad6e9b6
sailesh kumar <skumar@petz.net>
2023-07-09 12:27:39 +0530

    cosmatic change and change host port

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ COPY . .
 RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 EXPOSE 8000
 # Define the command to run when the container starts
-CMD ["python", "manage.py", "runserver", "172.23.0.2:8000"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- crudproject/settings.py
+++ crudproject/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'crudproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "crudOperation",
+        'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('POSTGRES_HOST'),
